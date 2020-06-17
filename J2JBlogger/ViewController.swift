f0c655aade29894e73c21b415a41f677e22663af
Poonam Lawand <poonam.lawand@searshc.com>
2020-06-17 20:55:54 +0530

    Initial Commit

---
+++ J2JBlogger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  J2JBlogger
+//
+//  Created by Lawand, Poonam on 17/06/20.
+//  Copyright © 2020 Lawand, Poonam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
